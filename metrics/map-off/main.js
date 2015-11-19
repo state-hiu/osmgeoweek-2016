a6489e31b570e88198c8e275a856938218520cef
d3netxer <tommygun320@yahoo.com>
2015-11-19 11:41:01 -0500

    adding metrics to top menu, adding marywash metrics page

--- metrics/map-off/main.js
+++ metrics/map-off/main.js
@@ -1,8 +1,11 @@
-//var root = 'http://184.169.128.35:8080';
-var root = 'http://localhost:8080';
+var root = 'http://184.169.128.35:8080';
+//var root = 'http://localhost:8080';
 var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/devseed.07f51987/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q', {
     minZoom: 2
 });
+
+var teamA = 'gmu';
+var teamB = 'hmsgw';
 
 //initiate map and set initial extent
 var map = L.map('map', { zoomControl: false })
@@ -12,7 +15,8 @@ var map = L.map('map', { zoomControl: false })
 
 var geojsonLayer = L.geoJson().addTo(map);
 
-var nextTimeline = [];
+var nextTimelineA = [];
+var nextTimelineB = [];
 var currentTimeline = [];
 var paused = false;
 var progressBarWidth = 0;
@@ -23,46 +27,90 @@ function reset () {
   $('#logroll').empty();
   $('#progress-bar').css('width', '0%');
 
-  currentTimeline = nextTimeline;
+  //currentTimeline = nextTimeline;
+
+  currentTimelineA = nextTimelineA;
+  currentTimelineB = nextTimelineB;
+
   progressBarWidth = currentTimeline.length;
-  currentTimeline.unshift('LAST');
+
+  //currentTimeline.unshift('LAST');
+
+  currentTimelineA.unshift('LAST');
+  currentTimelineB.unshift('LAST');
+
   currentProgress = 0;
 }
 
-$.get(root + '/timeline', function (timeline) {
+//how do you get two timelines?
+//http://stackoverflow.com/questions/9898813/jquery-wait-till-multiple-get-requests-are-successully-processed
+
+var nextTimelineA;
+var nextTimelineB;
+
+var timelineA = $.get(root + '/timeline' + '/gmu', function (timeline) {
     //The preprocessTimeline function is in a seperate file and pre-processes the timeline json
-    nextTimeline = preprocessTimeline(timeline);
+    nextTimelineA = preprocessTimeline(timeline);
+});
+
+var timelineB = $.get(root + '/timeline' + '/hmsgw', function (timeline) {
+    //The preprocessTimeline function is in a seperate file and pre-processes the timeline json
+    nextTimelineB = preprocessTimeline(timeline);
+});
+
+
+$.when(timelineA, timelineB).done(function() {
+
+    console.log('ok, we finished both get requests')
 
     //fills the Leaderboard with top 10 mappers by changes created
-    fillLeaderboard('changes');
+    fillLeaderboard('changes',teamA);
+    fillLeaderboard('changes',teamB);
+
+    console.log('leaderboards filled')
 
     reset();
 
     $('#spinner').hide();
+    $('#spinner2').hide();
 
     //renders every 3 seconds on the web map
+    var count = 0;
     setInterval(function () {
-      if (!paused) {
-        render(currentTimeline.pop());
+      count += 1;
+      if(count % 2 !== 0) {
+        if (!paused) {
+          render(currentTimelineA.pop(),teamA);
+        }
+      } else { 
+        if (!paused) {
+          render(currentTimelineB.pop(),teamB);
+        }
       }
     }, 3000);
 });
 
-function render (element) {
+
+function render (element,team) {
     //when you get to the last entry in the timeline, pause for 3 sec and get the next timeline
     if (element === 'LAST') {
       paused = true;
       setTimeout(function () {
         paused = false;
-        $.get(root + '/timeline', function (timeline) {
-          nextTimeline = preprocessTimeline(timeline);
+        $.get(root + '/timeline' + '/' + team, function (timeline) {
+          if (team == 'gmu') {
+            nextTimelineA = preprocessTimeline(timeline);
+          } else {
+            nextTimelineB = preprocessTimeline(timeline);
+          }
           reset();
         });
       }, 3000);
       return;
     }
 
-    var logroll = $('#logroll');
+
+    var logroll = $('#logroll'+team);
 
     var timecode = new Date(Date.parse(element.properties.created_at));
     var minutefix = timecode.getMinutes();
@@ -112,19 +160,23 @@ function render (element) {
 
 //after 5 minutes updates leaderboard with changes, this is a global variable
 var fillEvery5 = setInterval(function () {
-  fillLeaderboard('changes/MapGive');
+  fillLeaderboard('changes',teamA);
 }, 5 * 60 * 1000);
 
 //after 5 minutes updates leaderboard with changes, this is a global variable
 var fillEvery5 = setInterval(function () {
-  fillLeaderboard('changes/MissingMaps');
+  fillLeaderboard('changes',teamB);
 }, 5 * 60 * 1000);
 
-function fillLeaderboard (hash) {
-  $('#leaderboard').empty();
+//needs a second argument
+function fillLeaderboard (hash,schoolhash) {
+  
+//update this
+  $('#leaderboard-teamA').empty();
+  $('#leaderboard-teamB').empty();
   $('#Total').empty();
 
-  $.get(root + '/' + hash, function (data) {
+  $.get(root + '/' + hash + '/' + schoolhash, function (data) {
     for (var i = 2; i < data.length; i += 2) {
       var rank = (i / 2);
 
@@ -133,7 +185,7 @@ function fillLeaderboard (hash) {
         username = username.substring(0, 17) + '...';
       }
 
-      $('#leaderboard').append(
+      $('#leaderboard-teamA').append(
         '<li>' + rank + '.  ' + username + ' <i>' + numberFormat(data[i + 1],",") + '</i></li>'
       );
     }
@@ -161,25 +213,25 @@ function fillLeaderboard (hash) {
 
 //fills the Leaderboard with top 10 mappers by changes created when they click on the Leaderboard-All button
 $('#Leaderboard-All-teamA').click(function () {
-  fillLeaderboard('changes/MapGive');
+  fillLeaderboard('changes',teamA);
   return $('#leadertitletext').text('LEADERBOARDS');
 });
 
 //fills the Leaderboard with top 10 mappers by buildings created when they click on the Leaderboard-Building button
 $('#Leaderboard-Building-teamA').click(function () {
-  fillLeaderboard('buildings/MapGive');
+  fillLeaderboard('buildings',teamA);
   return $('#leadertitletext').text('BUILDINGS');
 });
 
 //fills the Leaderboard with top 10 mappers by highways created when they click on the Leaderboard-Roads button
 $('#Leaderboard-Roads-teamA').click(function () {
-  fillLeaderboard('highways/MapGive');
+  fillLeaderboard('highways',teamA);
   return $('#leadertitletext').text('ROADS');
 });
 
 //fills the Leaderboard with top 10 mappers by waterways created when they click on the Leaderboard-Rivers button
 $('#Leaderboard-Rivers-teamA').click(function () {
-  fillLeaderboard('waterways/MapGive');
+  fillLeaderboard('waterways',teamA);
   return $('#leadertitletext').text('RIVERS');
 });
 
@@ -187,25 +239,25 @@ $('#Leaderboard-Rivers-teamA').click(function () {
 
 //fills the Leaderboard with top 10 mappers by changes created when they click on the Leaderboard-All button
 $('#Leaderboard-All-teamB').click(function () {
-  fillLeaderboard('changes/MissingMaps');
+  fillLeaderboard('changes',teamB);
   return $('#leadertitletext').text('LEADERBOARDS');
 });
 
 //fills the Leaderboard with top 10 mappers by buildings created when they click on the Leaderboard-Building button
 $('#Leaderboard-Building-teamB').click(function () {
-  fillLeaderboard('buildings/MissingMaps');
+  fillLeaderboard('buildings',teamB);
   return $('#leadertitletext').text('BUILDINGS');
 });
 
 //fills the Leaderboard with top 10 mappers by highways created when they click on the Leaderboard-Roads button
 $('#Leaderboard-Roads-teamB').click(function () {
-  fillLeaderboard('highways/MissingMaps');
+  fillLeaderboard('highways',teamB);
   return $('#leadertitletext').text('ROADS');
 });
 
 //fills the Leaderboard with top 10 mappers by waterways created when they click on the Leaderboard-Rivers button
 $('#Leaderboard-Rivers-teamB').click(function () {
-  fillLeaderboard('waterways/MissingMaps');
+  fillLeaderboard('waterways',teamB);
   return $('#leadertitletext').text('RIVERS');
 });
 
